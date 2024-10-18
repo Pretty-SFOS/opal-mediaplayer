ff7024e366792c45cf20c4d6b22b5b4b5bbaf799
Mirian Margiani <mixosaurus+ichthyo@pm.me>
2024-10-18 22:48:34 +0200

    Add more translation files

---
+++ translations/opal-mediaplayer-fr.ts
@@ -0,0 +1,83 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+<context>
+    <name>Opal.MediaPlayer</name>
+    <message>
+        <location filename="../Opal/MediaPlayer/private/ErrorOverlay.qml" line="26"/>
+        <source>Playback failed</source>
+        <comment>error info page heading</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/MediaPlayer/private/ErrorOverlay.qml" line="30"/>
+        <source>Resource error</source>
+        <comment>error info page heading</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/MediaPlayer/private/ErrorOverlay.qml" line="32"/>
+        <source>Format error</source>
+        <comment>error info page heading</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/MediaPlayer/private/ErrorOverlay.qml" line="34"/>
+        <source>Network error</source>
+        <comment>error info page heading</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/MediaPlayer/private/ErrorOverlay.qml" line="36"/>
+        <source>Access denied</source>
+        <comment>error info page heading</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/MediaPlayer/private/ErrorOverlay.qml" line="38"/>
+        <source>Media service missing</source>
+        <comment>error info page heading</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/MediaPlayer/private/ErrorOverlay.qml" line="42"/>
+        <source>The video cannot be played due to a problem allocating resources.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/MediaPlayer/private/ErrorOverlay.qml" line="45"/>
+        <source>The audio and or video format is not supported.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/MediaPlayer/private/ErrorOverlay.qml" line="48"/>
+        <source>The video cannot be played due to network issues.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/MediaPlayer/private/ErrorOverlay.qml" line="51"/>
+        <source>The video cannot be played due to insufficient permissions.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/MediaPlayer/private/ErrorOverlay.qml" line="54"/>
+        <source>The video cannot be played because the media service could not be instantiated.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/MediaPlayer/private/ErrorOverlay.qml" line="57"/>
+        <source>Playback failed due to an expected error. Please restart the app and try again.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/MediaPlayer/VideoPlayerPage.qml" line="640"/>
+        <source>Zoomed to fit screen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/MediaPlayer/VideoPlayerPage.qml" line="641"/>
+        <source>Original</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
