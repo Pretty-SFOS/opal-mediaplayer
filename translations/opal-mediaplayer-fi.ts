5b9ac92102aaae857f96e7f6f4b9d52cc979b127
Elmeri Länsiharju <elmeri@lansiharju.fi>
2025-08-25 22:02:03 +0200

    Translated using Weblate (Finnish)
    Currently translated at 100.0% (22 of 22 strings)
    
    Co-authored-by: Elmeri Länsiharju <elmeri@lansiharju.fi>
    Translate-URL: https://hosted.weblate.org/projects/opal/opal-mediaplayer/fi/
    Translation: Opal for SailfishOS/Module: Opal.MediaPlayer

--- translations/opal-mediaplayer-fi.ts
+++ translations/opal-mediaplayer-fi.ts
@@ -4,118 +4,118 @@
 <context>
     <name>Opal.MediaPlayer</name>
     <message>
-        <location filename="../Opal/MediaPlayer/private/ErrorOverlay.qml" line="26"/>
+        <location filename="../Opal/MediaPlayer/private/ErrorOverlay.qml" line="26"></location>
         <source>Playback failed</source>
         <comment>error info page heading</comment>
         <translation>Toisto epäonnistui</translation>
     </message>
     <message>
-        <location filename="../Opal/MediaPlayer/private/ErrorOverlay.qml" line="30"/>
+        <location filename="../Opal/MediaPlayer/private/ErrorOverlay.qml" line="30"></location>
         <source>Resource error</source>
         <comment>error info page heading</comment>
         <translation>Resurssivirhe</translation>
     </message>
     <message>
-        <location filename="../Opal/MediaPlayer/private/ErrorOverlay.qml" line="32"/>
+        <location filename="../Opal/MediaPlayer/private/ErrorOverlay.qml" line="32"></location>
         <source>Format error</source>
         <comment>error info page heading</comment>
         <translation>Virhe tiedostomuodossa</translation>
     </message>
     <message>
-        <location filename="../Opal/MediaPlayer/private/ErrorOverlay.qml" line="34"/>
+        <location filename="../Opal/MediaPlayer/private/ErrorOverlay.qml" line="34"></location>
         <source>Network error</source>
         <comment>error info page heading</comment>
         <translation>Verkkovirhe</translation>
     </message>
     <message>
-        <location filename="../Opal/MediaPlayer/private/ErrorOverlay.qml" line="36"/>
+        <location filename="../Opal/MediaPlayer/private/ErrorOverlay.qml" line="36"></location>
         <source>Access denied</source>
         <comment>error info page heading</comment>
         <translation>Pääsy evätty</translation>
     </message>
     <message>
-        <location filename="../Opal/MediaPlayer/private/ErrorOverlay.qml" line="38"/>
+        <location filename="../Opal/MediaPlayer/private/ErrorOverlay.qml" line="38"></location>
         <source>Media service missing</source>
         <comment>error info page heading</comment>
         <translation>Mediapalvelu puuttuu</translation>
     </message>
     <message>
-        <location filename="../Opal/MediaPlayer/private/ErrorOverlay.qml" line="42"/>
+        <location filename="../Opal/MediaPlayer/private/ErrorOverlay.qml" line="42"></location>
         <source>The video cannot be played due to a problem allocating resources.</source>
         <translation>Videota ei voitu toistaa resurssien allokoinnissa ilmenneen ongelman vuoksi.</translation>
     </message>
     <message>
-        <location filename="../Opal/MediaPlayer/private/ErrorOverlay.qml" line="45"/>
+        <location filename="../Opal/MediaPlayer/private/ErrorOverlay.qml" line="45"></location>
         <source>The audio or video format is not supported.</source>
         <translation>Ääni- tai videoformaattia ei tueta.</translation>
     </message>
     <message>
-        <location filename="../Opal/MediaPlayer/private/ErrorOverlay.qml" line="48"/>
+        <location filename="../Opal/MediaPlayer/private/ErrorOverlay.qml" line="48"></location>
         <source>The video cannot be played due to network issues.</source>
-        <translation>Videota ei voitu toistaa verkkovirheen vuoksi.</translation>
+        <translation>Videota ei voida toistaa verkkovirheen vuoksi.</translation>
     </message>
     <message>
-        <location filename="../Opal/MediaPlayer/private/ErrorOverlay.qml" line="51"/>
+        <location filename="../Opal/MediaPlayer/private/ErrorOverlay.qml" line="51"></location>
         <source>The video cannot be played due to insufficient permissions.</source>
-        <translation>Videota ei voitu toistaa riittämättömien oikeuksien vuoksi.</translation>
+        <translation>Videota ei voida toistaa riittämättömien oikeuksien vuoksi.</translation>
     </message>
     <message>
-        <location filename="../Opal/MediaPlayer/private/ErrorOverlay.qml" line="54"/>
+        <location filename="../Opal/MediaPlayer/private/ErrorOverlay.qml" line="54"></location>
         <source>The video cannot be played because the media service could not be instantiated.</source>
         <translation>Videon toisto epäonnistui, koska mediapalvelua ei voitu määrittää.</translation>
     </message>
     <message>
-        <location filename="../Opal/MediaPlayer/private/ErrorOverlay.qml" line="57"/>
+        <location filename="../Opal/MediaPlayer/private/ErrorOverlay.qml" line="57"></location>
         <source>Playback failed due to an expected error. Please restart the app and try again.</source>
         <translation>Toisto epäonnistui odottamattoman virheen vuoksi. Sulje sovellus ja yritä uudelleen.</translation>
     </message>
     <message>
-        <location filename="../Opal/MediaPlayer/VideoPlayerPage.qml" line="208"/>
+        <location filename="../Opal/MediaPlayer/VideoPlayerPage.qml" line="208"></location>
         <source>Clear subtitles</source>
-        <translation>Poista tekstitys</translation>
+        <translation>Poista tekstitys käytöstä</translation>
     </message>
     <message>
-        <location filename="../Opal/MediaPlayer/VideoPlayerPage.qml" line="212"/>
+        <location filename="../Opal/MediaPlayer/VideoPlayerPage.qml" line="212"></location>
         <source>Load subtitles</source>
         <translation>Lisää tekstitystiedosto</translation>
     </message>
     <message>
-        <location filename="../Opal/MediaPlayer/VideoPlayerPage.qml" line="663"/>
+        <location filename="../Opal/MediaPlayer/VideoPlayerPage.qml" line="663"></location>
         <source>Zoomed to fit screen</source>
         <translation>Sovitettu näytölle</translation>
     </message>
     <message>
-        <location filename="../Opal/MediaPlayer/VideoPlayerPage.qml" line="664"/>
+        <location filename="../Opal/MediaPlayer/VideoPlayerPage.qml" line="664"></location>
         <source>Original</source>
         <translation>Alkuperäinen</translation>
     </message>
     <message>
-        <location filename="../Opal/MediaPlayer/VideoPlayerPage.qml" line="671"/>
+        <location filename="../Opal/MediaPlayer/VideoPlayerPage.qml" line="671"></location>
         <source>Play on repeat</source>
         <translation>Toista kertauksella</translation>
     </message>
     <message>
-        <location filename="../Opal/MediaPlayer/VideoPlayerPage.qml" line="672"/>
+        <location filename="../Opal/MediaPlayer/VideoPlayerPage.qml" line="672"></location>
         <source>Play once</source>
         <translation>Toista kerran</translation>
     </message>
     <message>
-        <location filename="../Opal/MediaPlayer/private/LoadSubtitlesDialog.qml" line="118"/>
+        <location filename="../Opal/MediaPlayer/private/LoadSubtitlesDialog.qml" line="118"></location>
         <source>Select from file system</source>
         <translation>Valitse tiedostoista</translation>
     </message>
     <message>
-        <location filename="../Opal/MediaPlayer/private/LoadSubtitlesDialog.qml" line="128"/>
+        <location filename="../Opal/MediaPlayer/private/LoadSubtitlesDialog.qml" line="128"></location>
         <source>No files ready</source>
         <translation>Ei tiedostoja saatavilla</translation>
     </message>
     <message>
-        <location filename="../Opal/MediaPlayer/private/LoadSubtitlesDialog.qml" line="131"/>
+        <location filename="../Opal/MediaPlayer/private/LoadSubtitlesDialog.qml" line="131"></location>
         <source>Copy subtitle files in the SRT format next to the video file to open them quickly.</source>
         <translation>Kopioi tekstitystiedostot SRT-muodossa videotiedoston yhteyteen avataksesi ne nopeasti.</translation>
     </message>
     <message>
-        <location filename="../Opal/MediaPlayer/private/LoadSubtitlesDialog.qml" line="135"/>
+        <location filename="../Opal/MediaPlayer/private/LoadSubtitlesDialog.qml" line="135"></location>
         <source>Pull down to pick a file.</source>
         <translation>Valitse tiedosto vetämällä alaspäin.</translation>
     </message>
